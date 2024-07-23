9ae3e0c5a08b7cc1653e7a53e5dd6452b8975d6f
Majok-Bol <bolmajok001@gmail.com>
2024-07-23 11:18:09 +0300

    created buttons and imported functions module

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("Hello world.")
+import { add, multiply, subtract, divide } from "./functions.js";
+console.log("Hello world.");
+console.log(add(12, 13));
+console.log(subtract(12, 13));
+console.log(divide(12, 13));
+console.log(multiply(12, 13));
