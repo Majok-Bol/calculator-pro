c9125bf66e17fe2439d2952fe3fa766ff1196250
Majok-Bol <bolmajok001@gmail.com>
2024-07-23 11:29:31 +0300

    added operation function and variables to store operators

--- functions.js
+++ functions.js
@@ -1,12 +1,25 @@
 export function add(a, b) {
-    return a + b;
-  }
-  export function subtract(a, b) {
-    return a - b;
-  }
-  export function divide(a, b) {
+  return a + b;
+}
+export function subtract(a, b) {
+  return a - b;
+}
+export function divide(a, b) {
+  return a / b;
+}
+export function multiply(a, b) {
+  return a * b;
+}
+export function operation(operator, a, b) {
+  if (operator === "+") {
+    return add(a, b);
+  } else if (operator === "-") {
+    return subtract(a, b);
+  } else if (operator === "/") {
     return a / b;
+  } else if (operator === "*") {
+    return multiply(a, b);
+  } else {
+    return "";
   }
-  export function multiply(a, b) {
-    return a * b;
-  }
+}
--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-import { add, multiply, subtract, divide } from "./functions.js";
+import { add, multiply, subtract, divide,operation } from "./functions.js";
 console.log("Hello world.");
 console.log(add(12, 13));
 console.log(subtract(12, 13));
 console.log(divide(12, 13));
 console.log(multiply(12, 13));
+// Create variables to hold the parts of the operation
+let firstNumber = "";
+let secondNumber = "";
+let currentOperator = "";
+let displayValue = "";
